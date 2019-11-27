d5e0094be1cc4450b7a10bb3495d6ffa316dffe5
thanakritfluk <thanakrit.da@ku.th>
2019-11-27 14:52:16 +0700

    Add navbar image and font

---
+++ public/javascripts/matches.js
@@ -0,0 +1,19 @@
+console.log("Js import success");
+var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+    [5, 2, 4, 2, 0]
+  ]
+};
+
+
+var option = {
+    width: 400,
+    height: 300
+};
+// Create a new line chart object where as first parameter we pass in a selector
+// that is resolving to our chart container element. The Second parameter
+// is the actual data object.
+new Chartist.Line('.ct-chart', data, option);
