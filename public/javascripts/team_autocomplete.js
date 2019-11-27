138eba908b7017ed2ab9817a55d5fc7585718d21
thanakritfluk <thanakrit.da@ku.th>
2019-11-27 21:38:10 +0700

    Add auto complete team name

---
+++ public/javascripts/team_autocomplete.js
@@ -0,0 +1,64 @@
+var substringMatcher = function (strs) {
+    return function findMatches(q, cb) {
+        var matches, substringRegex;
+
+        // an array that will be populated with substring matches
+        matches = [];
+
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i');
+
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function (i, str) {
+            if (substrRegex.test(str)) {
+                matches.push(str);
+            }
+        });
+
+        cb(matches);
+    };
+};
+
+var nba_team = ['Atlanta Hawks',
+    'Boston Celtics',
+    'Brooklyn Nets',
+    'Charlotte Hornets',
+    'Chicago Bulls',
+    'Cleveland Cavaliers',
+    'Dallas Mavericks',
+    'Denver Nuggets',
+    'Detroit Pistons',
+    'Golden State Warriors',
+    'Houston Rockets',
+    'Indiana Pacers',
+    'LA Clippers',
+    'Los Angeles Lakers',
+    'Memphis Grizzlies',
+    'Miami Heat',
+    'Milwaukee Bucks',
+    'Minnesota Timberwolves',
+    'New Orleans Pelicans',
+    'New York Knicks',
+    'Oklahoma City Thunder',
+    'Orlando Magic',
+    'Philadelphia 76ers',
+    'Phoenix Suns',
+    'Portland Trail Blazers',
+    'Sacramento Kings',
+    'San Antonio Spurs',
+    'Toronto Raptors',
+    'Utah Jazz',
+    'Washington Wizards'
+
+];
+
+$('.match_input').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        name: 'nba_team',
+        source: substringMatcher(nba_team)
+    });
