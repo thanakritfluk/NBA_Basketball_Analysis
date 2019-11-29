56e543af81795e8c9807cd41571b11a6bdd61543
Pichaaun Popukdee <pichaaun.p@ku.th>
2019-11-29 11:01:49 +0700

    mock json data and show on chart

--- public/javascripts/teamline_chart.js
+++ public/javascripts/teamline_chart.js
@@ -1,43 +1,69 @@
+// mock JSON file
+var jsonfile = {
+  "jsonarray": [{
+     "Teamname": "mock",
+     "rank": 10
+  }, {
+    "Teamname": "mock",
+    "rank": 7
+  },
+  {
+    "Teamname": "mock",
+    "rank": 8
+  },
+  {
+    "Teamname": "mock",
+    "rank": 3
+  },
+  {
+    "Teamname": "mock",
+    "rank": 5
+  },
+]
+};
+
+var d = new Date();
+var n = d.getFullYear();
+var labels = [n-4,n-3,n-2,n-1,n];
+var team_title = jsonfile.jsonarray[0].Teamname;
+// console.log(team_title);
+var data = jsonfile.jsonarray.map(function(e) {
+  return e.rank;
+});;
+
+var config = {
+  type: 'line',
+  data: {
+      labels: labels,
+      datasets: [{
+          fill:false,
+          lineTension: 0,
+          label: 'Ranking',
+          data: data,
+          backgroundColor: [
+             'rgba(252, 126, 9,1)'
+          ],
+          borderColor: [
+            'rgba(252, 126, 9,1)'
+          ],
+          borderWidth: 0
+      }]
+  },
+  options: {
+      title: {
+        display: true,
+        text: team_title ,
+        fontSize: 16
+      },
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  }
+};
+
 var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['2014','2015','2016','2018','2019'],
-        datasets: [{
-            fill:false,
-            label: 'Ranking',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        title: {
-          display: true,
-          text: 'Selected team ranking in past 5 years',
-          fontSize: 16
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+var myChart = new Chart(ctx, config);
